eb39da8721f600e98d5faa6b35438b2dddf02487
Myroslav Martynov <marty.mircha@gmail.com>
2024-04-11 01:24:33 +0200

    feat: 🎸 web component for event plus test data

--- src/js/event-component.js
+++ src/js/event-component.js
@@ -1,3 +1,61 @@
-export function EventComponent() {
-  console.log("EventComponent");
+export class EventComponent extends HTMLElement {
+  render() {
+    this.innerHTML = `
+      <style>
+        .event-component {
+          background-color: rgba(50, 0, 255, 0.75);
+          border-radius: 0.25rem;
+          position: absolute;
+          overflow: hidden;
+          display: flex;
+          padding: 0 0.25rem;
+        }
+        
+        .event-component__title {
+          font-size: 0.75rem;
+          color: white;
+        }
+      </style>
+      <div class="event-component" style="width: ${this.getAttribute(
+        "width"
+      )}; top: ${this.top}%; height: ${this.height}%">
+        <span class="event-component__title">
+        ${this.getAttribute("title")}
+        </span>
+      </div>
+    `;
+  }
+
+  get top() {
+    if (!this.hasAttribute("start")) {
+      throw new Error("start attribute is required");
+    }
+
+    return (100 / 24) * this.getAttribute("start");
+  }
+
+  get height() {
+    if (!this.hasAttribute("start") || !this.hasAttribute("end")) {
+      throw new Error("start and end attributes are required");
+    }
+
+    return (100 / 24) * (this.getAttribute("end") - this.getAttribute("start"));
+  }
+
+  connectedCallback() {
+    if (!this.rendered) {
+      this.render();
+      this.rendered = true;
+    }
+  }
+
+  static get observedAttributes() {
+    return ["width", "start", "end", "title"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
 }
+
+customElements.define("event-component", EventComponent);
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,54 @@
 import { EventComponent } from "./event-component.js";
 
+const EVENTS_TEST_DATA = [
+  {
+    start: 1,
+    end: 2,
+    title: "Event 1",
+  },
+  {
+    start: 2,
+    end: 3,
+    title: "Event 2",
+  },
+  {
+    start: 3,
+    end: 4,
+    title: "Event 3",
+  },
+  {
+    start: 8,
+    end: 11,
+    title: "Long event",
+  },
+  {
+    start: 10,
+    end: 12,
+    title: "overlapping event",
+  },
+  {
+    start: 11.5,
+    end: 13,
+    title: "overlapping event 2",
+  },
+  {
+    start: 16,
+    end: 16.5,
+    title: "Short event",
+  },
+];
+
 export default function main() {
-  console.log("main");
-  EventComponent();
+  // NOTE: for PoC it's hardcoded to Thursday
+  const thursday = document.getElementById("cal-thursday");
+  thursday.append(
+    ...EVENTS_TEST_DATA.map((event) => {
+      const eventComponent = new EventComponent();
+      eventComponent.setAttribute("start", event.start);
+      eventComponent.setAttribute("end", event.end);
+      eventComponent.setAttribute("title", event.title);
+      eventComponent.setAttribute("width", "100%");
+      return eventComponent;
+    })
+  );
 }
